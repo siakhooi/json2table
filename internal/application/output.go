aaff6f83637619271b70d03454708cbc948b9043
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2026-02-20 12:14:08 +0000

    print nil if data not found

--- internal/application/output.go
+++ internal/application/output.go
@@ -22,22 +22,27 @@ func selectDataArray(dataPath string, fullData interface{}) ([]interface{}, erro
 	return nil, fmt.Errorf("data selected with jsonpath is not an array")
 }
 
-// PrintTable prints the spec and JSON data in a pretty format
+func printHeader(columns []Column) {
+	for _, column := range columns {
+		fmt.Printf("%*s ", column.Width, column.Title)
+	}
+	fmt.Println("")
+}
+
+// PrintTable prints JSON data in tabular format based on the provided specification
 func PrintTable(spec *Spec, fullData interface{}) error {
 	dataArray, err := selectDataArray(spec.DataPath, fullData)
 	if err != nil {
 		return err
 	}
 
-	for _, column := range spec.Columns {
-		fmt.Printf("%*s ", column.Width, column.Title)
-	}
-	fmt.Println("")
+	printHeader(spec.Columns)
+
 	for _, item := range dataArray {
 		for _, column := range spec.Columns {
 			value, err := jsonpath.Get(column.Path, item)
 			if err != nil {
-				return fmt.Errorf("error selecting column data with jsonpath: %w", err)
+				value = nil
 			}
 			fmt.Printf("%*v ", column.Width, value)
 		}
