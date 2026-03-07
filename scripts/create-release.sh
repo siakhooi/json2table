ca082a99679e03d4ed5e71e3b66f6fcdb2cd01ce
Siak Hooi <siakhooi@gmail.com>
2026-03-07 10:40:27 +0000

    v0.5.0

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -4,7 +4,5 @@ set -e
 # shellcheck disable=SC1091
 . ./release.env
 
-RELEASE_NOTE="$RELEASE_TITLE"
-
 set -x
 gh release create "$RELEASE_VERSION" --title "$RELEASE_TITLE" --notes "${RELEASE_NOTE}" --latest
