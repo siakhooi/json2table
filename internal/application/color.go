0c648c3fec126a49b867cbf91d4460b02830c40a
Siak Hooi <siakhooi@gmail.com>
2026-03-07 09:17:41 +0000

    hi, bg, hiBg colors

--- internal/application/color.go
+++ internal/application/color.go
@@ -9,6 +9,9 @@ import "github.com/fatih/color"
 type SupportedColor string
 
 const (
+	// ColorDefault represents default color (no color)
+	ColorDefault SupportedColor = "default"
+
 	// ColorRed represents red color
 	ColorRed SupportedColor = "red"
 	// ColorGreen represents green color
@@ -25,8 +28,57 @@ const (
 	ColorWhite SupportedColor = "white"
 	// ColorBlack represents black color
 	ColorBlack SupportedColor = "black"
-	// ColorDefault represents default color (no color)
-	ColorDefault SupportedColor = "default"
+
+	// ColorHiRed represents high intensity red color
+	ColorHiRed SupportedColor = "hiRed"
+	// ColorHiGreen represents high intensity green color
+	ColorHiGreen SupportedColor = "hiGreen"
+	// ColorHiBlue represents high intensity blue color
+	ColorHiBlue SupportedColor = "hiBlue"
+	// ColorHiYellow represents high intensity yellow color
+	ColorHiYellow SupportedColor = "hiYellow"
+	// ColorHiMagenta represents high intensity magenta color
+	ColorHiMagenta SupportedColor = "hiMagenta"
+	// ColorHiCyan represents high intensity cyan color
+	ColorHiCyan SupportedColor = "hiCyan"
+	// ColorHiWhite represents high intensity white color
+	ColorHiWhite SupportedColor = "hiWhite"
+	// ColorHiBlack represents high intensity black color
+	ColorHiBlack SupportedColor = "hiBlack"
+
+	// ColorBgRed represents red background color
+	ColorBgRed SupportedColor = "bgRed"
+	// ColorBgGreen represents green background color
+	ColorBgGreen SupportedColor = "bgGreen"
+	// ColorBgBlue represents blue background color
+	ColorBgBlue SupportedColor = "bgBlue"
+	// ColorBgYellow represents yellow background color
+	ColorBgYellow SupportedColor = "bgYellow"
+	// ColorBgMagenta represents magenta background color
+	ColorBgMagenta SupportedColor = "bgMagenta"
+	// ColorBgCyan represents cyan background color
+	ColorBgCyan SupportedColor = "bgCyan"
+	// ColorBgWhite represents white background color
+	ColorBgWhite SupportedColor = "bgWhite"
+	// ColorBgBlack represents black background color
+	ColorBgBlack SupportedColor = "bgBlack"
+
+	// ColorHiBgRed represents high intensity red background color
+	ColorHiBgRed SupportedColor = "hiBgRed"
+	// ColorHiBgGreen represents high intensity green background color
+	ColorHiBgGreen SupportedColor = "hiBgGreen"
+	// ColorHiBgBlue represents high intensity blue background color
+	ColorHiBgBlue SupportedColor = "hiBgBlue"
+	// ColorHiBgYellow represents high intensity yellow background color
+	ColorHiBgYellow SupportedColor = "hiBgYellow"
+	// ColorHiBgMagenta represents high intensity magenta background color
+	ColorHiBgMagenta SupportedColor = "hiBgMagenta"
+	// ColorHiBgCyan represents high intensity cyan background color
+	ColorHiBgCyan SupportedColor = "hiBgCyan"
+	// ColorHiBgWhite represents high intensity white background color
+	ColorHiBgWhite SupportedColor = "hiBgWhite"
+	// ColorHiBgBlack represents high intensity black background color
+	ColorHiBgBlack SupportedColor = "hiBgBlack"
 )
 
 type colorMeta struct {
@@ -34,15 +86,39 @@ type colorMeta struct {
 }
 
 var supportedColorMeta = map[SupportedColor]colorMeta{
-	ColorRed:     {color: color.FgRed},
-	ColorGreen:   {color: color.FgGreen},
-	ColorBlue:    {color: color.FgBlue},
-	ColorYellow:  {color: color.FgYellow},
-	ColorMagenta: {color: color.FgMagenta},
-	ColorCyan:    {color: color.FgCyan},
-	ColorWhite:   {color: color.FgWhite},
-	ColorBlack:   {color: color.FgBlack},
-	ColorDefault: {color: color.Reset},
+	ColorDefault:     {color: color.Reset},
+	ColorRed:         {color: color.FgRed},
+	ColorGreen:       {color: color.FgGreen},
+	ColorBlue:        {color: color.FgBlue},
+	ColorYellow:      {color: color.FgYellow},
+	ColorMagenta:     {color: color.FgMagenta},
+	ColorCyan:        {color: color.FgCyan},
+	ColorWhite:       {color: color.FgWhite},
+	ColorBlack:       {color: color.FgBlack},
+	ColorHiRed:       {color: color.FgHiRed},
+	ColorHiGreen:     {color: color.FgHiGreen},
+	ColorHiBlue:      {color: color.FgHiBlue},
+	ColorHiYellow:    {color: color.FgHiYellow},
+	ColorHiMagenta:   {color: color.FgHiMagenta},
+	ColorHiCyan:      {color: color.FgHiCyan},
+	ColorHiWhite:     {color: color.FgHiWhite},
+	ColorHiBlack:     {color: color.FgHiBlack},
+	ColorBgRed:       {color: color.BgRed},
+	ColorBgGreen:     {color: color.BgGreen},
+	ColorBgBlue:      {color: color.BgBlue},
+	ColorBgYellow:    {color: color.BgYellow},
+	ColorBgMagenta:   {color: color.BgMagenta},
+	ColorBgCyan:      {color: color.BgCyan},
+	ColorBgWhite:     {color: color.BgWhite},
+	ColorBgBlack:     {color: color.BgBlack},
+	ColorHiBgRed:     {color: color.BgHiRed},
+	ColorHiBgGreen:   {color: color.BgHiGreen},
+	ColorHiBgBlue:    {color: color.BgHiBlue},
+	ColorHiBgYellow:  {color: color.BgHiYellow},
+	ColorHiBgMagenta: {color: color.BgHiMagenta},
+	ColorHiBgCyan:    {color: color.BgHiCyan},
+	ColorHiBgWhite:   {color: color.BgHiWhite},
+	ColorHiBgBlack:   {color: color.BgHiBlack},
 }
 
 // GetColored returns the printValue wrapped in color codes based on the color string
