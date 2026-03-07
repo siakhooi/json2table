4a2ce6cd84bc0c3f7c5f36252a9d270c485fa34b
Siak Hooi <siakhooi@gmail.com>
2026-03-07 08:03:40 +0000

    add all fg colors

--- internal/application/color.go
+++ internal/application/color.go
@@ -15,6 +15,16 @@ const (
 	ColorGreen SupportedColor = "green"
 	// ColorBlue represents blue color
 	ColorBlue SupportedColor = "blue"
+	// ColorYellow represents yellow color
+	ColorYellow SupportedColor = "yellow"
+	// ColorMagenta represents magenta color
+	ColorMagenta SupportedColor = "magenta"
+	// ColorCyan represents cyan color
+	ColorCyan SupportedColor = "cyan"
+	// ColorWhite represents white color
+	ColorWhite SupportedColor = "white"
+	// ColorBlack represents black color
+	ColorBlack SupportedColor = "black"
 	// ColorDefault represents default color (no color)
 	ColorDefault SupportedColor = "default"
 )
@@ -23,17 +33,19 @@ type colorMeta struct {
 	color color.Attribute
 }
 
-// SupportedColorMeta is a list of supported colors
-var SupportedColorMeta = map[SupportedColor]colorMeta{
-	ColorRed:   {color: color.FgRed},
-	ColorGreen: {color: color.FgGreen},
-	ColorBlue:  {color: color.FgBlue},
+var supportedColorMeta = map[SupportedColor]colorMeta{
+	ColorRed:     {color: color.FgRed},
+	ColorGreen:   {color: color.FgGreen},
+	ColorBlue:    {color: color.FgBlue},
+	ColorYellow:  {color: color.FgYellow},
+	ColorMagenta: {color: color.FgMagenta},
+	ColorCyan:    {color: color.FgCyan},
+	ColorWhite:   {color: color.FgWhite},
+	ColorBlack:   {color: color.FgBlack},
+	ColorDefault: {color: color.Reset},
 }
 
 // GetColored returns the printValue wrapped in color codes based on the color string
 func GetColored(printValue string, s SupportedColor) any {
-	if s == ColorDefault {
-		return printValue
-	}
-	return color.New(SupportedColorMeta[s].color).SprintFunc()(printValue)
+	return color.New(supportedColorMeta[s].color).SprintFunc()(printValue)
 }
